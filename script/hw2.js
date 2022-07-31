34f017c3b4c22eefb4118ae53182246d49386f10
Oxumoron <oxymoron.dr@gmail.com>
2022-07-31 13:31:33 +0200

    1

---
+++ script/hw2.js
@@ -0,0 +1,16 @@
+//      1
+const citiesAndCountries = {
+	'Киев': 'Украина',
+	'Нью-Йорк': 'США',
+	'Амстердам': 'Нидерланды',
+	'Берлин': 'Германия',
+	'Париж': 'Франция',
+	'Лиссабон': 'Португалия',
+	'Вена': 'Австрия',
+};
+
+const result = [];
+for (var prop in citiesAndCountries) {
+  result.push(prop + " - это " + citiesAndCountries[prop]);
+}
+console.log(result);
