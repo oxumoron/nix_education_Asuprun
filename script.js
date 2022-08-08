f645088d7c4ac067c6ad84bc855b4f051264b044
Oxumoron <oxymoron.dr@gmail.com>
2022-08-08 09:57:22 +0200

    2

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// import { studentArr } from "./studentArr.js";
+
 // Задача - создать класс Student который принимает аргументом в конструкторе объект enrollee (абитурент). У экземпляра класса Student должны быть поля:
 // id - уникальный идентификатор студента (генерируется при создании экземпляра и начинается с 1);
 // name - имя студента (передаем в объекте enrollee);
@@ -10,86 +12,6 @@
 
 
 // const studentsArr = [... массив со студентами].
-
-const studentArr = [
-  {
-    name: 'Сергей',
-    surname: 'Войлов',
-    ratingPoint: 1000,
-    schoolPoint: 1000,
-    course: 2,
-  },
-  {
-    name: 'Татьяна',
-    surname: 'Коваленко',
-    ratingPoint: 880,
-    schoolPoint: 700,
-    course: 1,
-  },
-  {
-    name: 'Анна',
-    surname: 'Кугир',
-    ratingPoint: 1430,
-    schoolPoint: 1200,
-    course: 3,
-  },
-  {
-    name: 'Станислав',
-    surname: 'Щелоков',
-    ratingPoint: 1130,
-    schoolPoint: 1060,
-    course: 2,
-  },
-  {
-    name: 'Денис',
-    surname: 'Хрущ',
-    ratingPoint: 1000,
-    schoolPoint: 990,
-    course: 4,
-  },
-  {
-    name: 'Татьяна',
-    surname: 'Капустник',
-    ratingPoint: 650,
-    schoolPoint: 500,
-    course: 3,
-  },
-  {
-    name: 'Максим',
-    surname: 'Меженский',
-    ratingPoint: 990,
-    schoolPoint: 1100,
-    course: 1,
-  },
-  {
-    name: 'Денис',
-    surname: 'Марченко',
-    ratingPoint: 570,
-    schoolPoint: 1300,
-    course: 4,
-  },
-  {
-    name: 'Антон',
-    surname: 'Завадский',
-    ratingPoint: 1090,
-    schoolPoint: 1010,
-    course: 3
-  },
-  {
-    name: 'Игорь',
-    surname: 'Куштым',
-    ratingPoint: 870,
-    schoolPoint: 790,
-    course: 1,
-  },
-  {
-    name: 'Инна',
-    surname: 'Скакунова',
-    ratingPoint: 1560,
-    schoolPoint: 200,
-    course: 2,
-  }
-]
 
 class Student {
   static _id = 1
@@ -141,10 +63,11 @@ class CustomString{
     return console.log(str[0].toUpperCase() + str.slice(1));
   }
   ucWords(str) {
-    return str
+    return console.log(str
       .split(' ')
-      .map(word => this.ucFirst(word))
+      .map(word => word[0].toUpperCase() + word.slice(1))
       .join(' ')
+      )
   }
 }
 
@@ -152,4 +75,4 @@ const myString = new CustomString();
 
 myString.reverse('qwerty'); //выведет 'ytrewq'
 myString.ucFirst('qwerty'); //выведет 'Qwerty'
-myString.ucWords('qwerty wwerty ewerty'); //выведет 'Qwerty Qwerty Qwerty
+myString.ucWords('qwerty qwerty qwerty'); //выведет 'Qwerty Qwerty Qwerty
