1e050c350232b7ef3dac7be3c3a627c24cf8d50b
Oxumoron <oxymoron.dr@gmail.com>
2022-08-10 20:42:52 +0200

    2.2

--- script.js
+++ script.js
@@ -28,6 +28,27 @@ const getUpdatedArr = updateArr()
 
 getUpdatedArr(3) // [3]
 getUpdatedArr(5) // [3, 5]
-getUpdatedArr({name: 'Vasya'}); 
+getUpdatedArr({name: 'Vasya'});  // [3, 5, {name: 'Vasya'}]
 getUpdatedArr() // []
 getUpdatedArr(4)
+
+
+
+//        2.2
+function updateStr(data){
+  let str = ''
+
+  return function (data){
+    data ? str += data : str = ''
+
+    return console.log(str);
+  }
+}
+
+const getUpdatedStr = updateStr()
+getUpdatedStr('a') // [3]
+getUpdatedStr('b') // [3, 5]
+getUpdatedStr('bfafafaaf') // [3, 5]
+getUpdatedStr('bfaafafafnnnnnn') // [3, 5]
+getUpdatedStr() // [3, 5]
+getUpdatedStr('b') // [3, 5]
