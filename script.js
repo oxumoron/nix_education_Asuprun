e94a209af0db47bf9f38f479a5fa366df56d3f69
Oxumoron <oxymoron.dr@gmail.com>
2022-08-10 11:12:12 +0200

    2

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ counter(3);
 counter(5);
 counter(228);
 
+//        2
+function updateArr(data){
+  let arr = []
+
+  return function (data){
+    data ? arr.push(data) : arr = []
+
+    return console.log(arr);
+  }
+}
+
+const getUpdatedArr = updateArr()
+
+getUpdatedArr(3) // [3]
+getUpdatedArr(5) // [3, 5]
+getUpdatedArr({name: 'Vasya'}); 
+getUpdatedArr() // []
+getUpdatedArr(4)
