d95341d23088858dc3ffbb54b3c21659c897554c
Oxumoron <oxymoron.dr@gmail.com>
2022-08-23 13:27:52 +0200

    try like + heart

--- js/main.js
+++ js/main.js
@@ -9,6 +9,9 @@ const newOrders = document.getElementById('product__orders');
 const newPrice = document.getElementById('product__price');
 const newReviews = document.getElementById('product__reviews');
 const newStock = document.getElementById('product__stock');
+const like = document.getElementById('like');
+like.addEventListener('click', (filled) => like.classList.toggle('filled'));
+console.log(like);
 
 
 items.forEach((el) => {
@@ -17,12 +20,35 @@ items.forEach((el) => {
   newImage.src = `./img/${el.imgUrl}`;
   newName.textContent = el.name;
   newStock.textContent = el.orderInfo.inStock;
-  // if(el.orderInfo.inStock === 0){
-  //   newStock.className = 'nostock'
-  // }
+  if(el.orderInfo.inStock === 0){
+    newStock.classList.add('nostock')
+  }
   newPrice.textContent = el.price;
   newOrders.textContent = Math.floor(300 + 700 * Math.random());
   newReviews.textContent = el.orderInfo.reviews;
   newCard.innerHTML = productWrapper.innerHTML;
   products.appendChild(newCard);
 });
+// console.log(document.getElementById('like'));
+
+
+// if (localStorage.prompt1) {
+//   document.getElementById('input').value = localStorage.prompt1;
+// }else{
+//   localStorage.prompt1
+// }
+// localStorage.prompt1 = prompt('Enter user name')
+
+// let result = new Promise((resolve, reject) => {
+   
+//   setTimeout(() => {
+//        resolve(value);
+//      else
+//        reject(value);
+//    }, 1000);
+//   });
+  
+//   result
+//     .then(result => console.log('Success: ', result))
+//     .catch(error => console.log('Error: ', error))
+//     .finally(() => console.log('JavaScript Promise finished'));
