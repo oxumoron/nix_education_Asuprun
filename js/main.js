60aa0f6674e2c3bbb17373f123537554f508c87c
Oxumoron <oxymoron.dr@gmail.com>
2022-08-25 15:40:36 +0200

    accord

--- js/main.js
+++ js/main.js
@@ -11,8 +11,7 @@ const newReviews = document.getElementById('product__reviews');
 const newStock = document.getElementById('product__stock');
 const like = document.getElementById('like');
 like.addEventListener('click', (filled) => like.classList.toggle('filled'));
-console.log(like);
-
+// console.log(like);
 
 items.forEach((el) => {
   let newCard = document.createElement("li");
@@ -20,15 +19,72 @@ items.forEach((el) => {
   newImage.src = `./img/${el.imgUrl}`;
   newName.textContent = el.name;
   newStock.textContent = el.orderInfo.inStock;
-  if(+newStock.textContent === 0){
-    newStock.classList.add('nostock')
-  }
+  // if(+newStock.textContent === 0){
+  //   newStock.classList.add('nostock')
+  // }
   newPrice.textContent = el.price;
   newOrders.textContent = Math.floor(300 + 700 * Math.random());
   newReviews.textContent = el.orderInfo.reviews;
   newCard.innerHTML = productWrapper.innerHTML;
   products.appendChild(newCard);
 });
+
+
+const colList = document.getElementById('color-cat__list');
+const memList = document.getElementById('memory-cat__list');
+const osList = document.getElementById('os-cat__list');
+
+
+let colorAll = [];
+let memoryAll = [];
+let osAll = [];
+
+items.map(item => {
+  colorAll.push(...item.color);
+  memoryAll.push(item.storage);
+  osAll.push(item.os)
+})
+
+colorAll = [... new Set(colorAll)];
+memoryAll = [... new Set(memoryAll)];
+osAll = [... new Set(osAll)];
+// const newResult = new Set(result)
+
+colorAll.map(elem => {
+  let newCol = document.createElement("li");
+      newCol.classList.add('cat__item');
+      newCol.innerHTML = `
+          <label class="item__label" for="${elem}">${elem}</label>
+          <input class="item__check" type="checkbox" id="${elem}">
+      `
+
+      colList.appendChild(newCol);
+});
+
+memoryAll.map(elem => {
+  let newMem = document.createElement("li");
+      newMem.classList.add('cat__item');
+      newMem.innerHTML = `
+          <label class="item__label" for="${elem}">${elem}</label>
+          <input class="item__check" type="checkbox" id="${elem}">
+      `
+
+      memList.appendChild(newMem);
+});
+
+osAll.map(elem => {
+  let newOs = document.createElement("li");
+      newOs.classList.add('cat__item');
+      newOs.innerHTML = `
+          <label class="item__label" for="${elem}">${elem}</label>
+          <input class="item__check" type="checkbox" id="${elem}">
+      `
+
+      osList.appendChild(newOs);
+});
+
+// console.log(colorAll);
+
 
 // console.log(newStock.textContent);
 
