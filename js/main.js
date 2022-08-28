55ea4b9fbc2874a93e85f30ad46f880fb6a0ae7c
Oxumoron <oxymoron.dr@gmail.com>
2022-08-28 14:00:09 +0200

    add modal

--- js/main.js
+++ js/main.js
@@ -9,19 +9,19 @@ const newOrders = document.getElementById('product__orders');
 const newPrice = document.getElementById('product__price');
 const newReviews = document.getElementById('product__reviews');
 const newStock = document.getElementById('product__stock');
-const like = document.getElementById('like');
-like.addEventListener('click', (filled) => like.classList.toggle('filled'));
-// console.log(like);
 
 items.forEach((el) => {
   let newCard = document.createElement("li");
+  let id = el.id;
   newCard.classList = "product__wrapper";
+  newCard.id = `${id}`;
   newImage.src = `./img/${el.imgUrl}`;
   newName.textContent = el.name;
   newStock.textContent = el.orderInfo.inStock;
-  // if(+newStock.textContent === 0){
-  //   newStock.classList.add('nostock')
-  // }
+
+  if(+newStock.textContent === 0){
+    newStock.classList.add('nostock')
+  }
   newPrice.textContent = el.price;
   newOrders.textContent = Math.floor(300 + 700 * Math.random());
   newReviews.textContent = el.orderInfo.reviews;
@@ -48,7 +48,6 @@ items.map(item => {
 colorAll = [... new Set(colorAll)];
 memoryAll = [... new Set(memoryAll)];
 osAll = [... new Set(osAll)];
-// const newResult = new Set(result)
 
 colorAll.map(elem => {
   let newCol = document.createElement("li");
@@ -92,7 +91,6 @@ accordBtn.addEventListener('click', (event) => {
   event.currentTarget.classList.toggle('active');
 });
 
-// console.log(colorAll);
 
 
 // console.log(newStock.textContent);
@@ -117,3 +115,56 @@ accordBtn.addEventListener('click', (event) => {
 //     .then(result => console.log('Success: ', result))
 //     .catch(error => console.log('Error: ', error))
 //     .finally(() => console.log('JavaScript Promise finished'));
+
+const popup = document.getElementById('product__modal');
+const productWrappers = document.querySelectorAll('li.product__wrapper');
+const tagBody = document.querySelector('body')
+
+productWrappers.forEach((item) => {
+  item.addEventListener('click', () => {
+    popup.classList.add('modal__active')
+    tagBody.classList.add('hidden');
+      
+      const popupImg =  document.getElementById('popup-img');
+      const popupReview =  document.getElementById('col__review');
+      const popupOrders =  document.getElementById('col__orders');
+      const popupName =  document.getElementById('col__title');
+      const popupColor =  document.getElementById('col-color');
+      const popupOs =  document.getElementById('col-os');
+      const popupChip =  document.getElementById('col-chip');
+      const popupHeight =  document.getElementById('col-height');
+      const popupWidth =  document.getElementById('col-width');
+      const popupDepth =  document.getElementById('col-depth');
+      const popupWeight =  document.getElementById('col-weight');
+      const popupPrice =  document.getElementById('col-price');
+      const popupStock =  document.getElementById('col-stock');
+
+      items.find((el) => {
+        if(+el.id === +item.id) {
+        popupImg.src = `./img/${el.imgUrl}`;
+        popupReview.textContent = el.orderInfo.reviews;
+        popupOrders.textContent = el.orderInfo.inStock;
+        popupName.textContent = el.name;
+        popupColor.textContent = el.color;
+        popupOs.textContent = el.os;
+        popupChip.textContent = el.chip.name;
+        popupHeight.textContent = el.size.height;
+        popupWidth.textContent = el.size.width;
+        popupDepth.textContent = el.size.depth;
+        popupWeight.textContent = el.size.weight;
+        popupPrice.textContent = el.price;
+        popupStock.textContent = el.orderInfo.inStock;
+      }
+    });
+
+
+    popup.onmousedown = function (e) {
+        let target = e.target;
+        let modalContent = popup.getElementsByClassName('modal__inner')[0];
+        if (e.target.closest('.' + modalContent.className) === null) {
+          this.classList.remove('modal__active');
+          tagBody.classList.remove('hidden');
+        }
+      };
+    }
+)})
