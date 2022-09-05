4581e44b4036b76962afe9cb0eb92da7e715f4b6
Oxumoron <oxymoron.dr@gmail.com>
2022-09-05 15:59:07 +0200

    add like

--- js/main.js
+++ js/main.js
@@ -44,7 +44,9 @@ const createCards = function (card) {
 
     newCard.innerHTML = `
               <div class="product">
-                  <img id="like" src="./img/icons/like_empty.svg" alt="" class="like">
+                  <div class="like">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="24px" height="24px"><path d="M0 0h24v24H0z" fill="none"/><path d="M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3zm-4.4 15.55l-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05z"/></svg>
+                  </div>
                   <div class="product__photo">
                     <img class="product__img" id="product__photo" src="${newImage}" height="200" width="200" alt="">
                   </div>
@@ -118,6 +120,8 @@ colorAll.map(elem => {
   colList.appendChild(newCol);
 });
 
+
+
 memoryAll.map(elem => {
   let newMem = document.createElement("li");
   newMem.classList.add('cat__item');
@@ -152,6 +156,20 @@ accordBtn.addEventListener('click', (event) => {
 const popup = document.getElementById('product__modal');
 const productWrappers = document.querySelectorAll('li.product__wrapper');
 const tagBody = document.querySelector('body')
+
+productWrappers.forEach((item) => {
+  item.addEventListener('click', (event) => {
+    const {target} = event;
+    if (target.parentElement.className === "like"){
+      const like = document.querySelectorAll('.like');
+      like.forEach(el => {
+        el.addEventListener('click', (event) => {
+          el.classList.toggle('filled');
+        })
+      })
+    }
+  })
+})
 
 // const cartPopup = document.getElementById('cart-modal');
 const arrBtnToAddCart = document.querySelectorAll('.btn');
