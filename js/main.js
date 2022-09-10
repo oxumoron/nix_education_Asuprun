d8e3620a0db5af83b3085b23d4fc8269fae37b25
Oxumoron <oxymoron.dr@gmail.com>
2022-09-10 16:31:58 +0200

    last

--- js/main.js
+++ js/main.js
@@ -45,7 +45,7 @@ const createCards = function (card) {
     newCard.innerHTML = `
               <div class="product">
                   <div class="like">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="24px" height="24px"><path d="M0 0h24v24H0z" fill="none"/><path d="M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3zm-4.4 15.55l-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05z"/></svg>
+                  <img src="img/icons/like_empty.svg" alt="">
                   </div>
                   <div class="product__photo">
                     <img class="product__img" id="product__photo" src="${newImage}" height="200" width="200" alt="">
@@ -69,7 +69,7 @@ const createCards = function (card) {
     // console.log(newStock === 0);
 
     products.appendChild(newCard);
-
+    
   });
 }
 
@@ -164,6 +164,7 @@ productWrappers.forEach((item) => {
       const like = document.querySelectorAll('.like');
       like.forEach(el => {
         el.addEventListener('click', (event) => {
+          event.stopPropagation();
           el.classList.toggle('filled');
         })
       })
@@ -178,7 +179,7 @@ const itemBox = document.querySelectorAll('li.product__wrapper');
 const cartCont = document.getElementById('cart-modal');
 
 function addToCart(e) {
-  this.disabled = true;
+  // this.disabled = true;
   let cartData = getCartData() || {},
     parentBox = this.parentNode.parentNode.parentNode,
     itemId = this.getAttribute('data-id'),
@@ -192,9 +193,8 @@ function addToCart(e) {
   }
   
   if (!setCartData(cartData)) {
-    this.disabled = false;
+    // this.disabled = false;
   }
-  return false;
 }
 for (let i = 0; i < itemBox.length; i++) {
   itemBox[i].querySelector('.btn').addEventListener('click', addToCart)
@@ -259,7 +259,6 @@ function openCart(e) {
     `;
   }
 
-  return false;
 }
 
 document.getElementById('cart').addEventListener('click', (openCart));
@@ -344,6 +343,7 @@ function popupFunc(array) {
           tagBody.classList.remove('hidden');
         }
       };
+
     })
   })
 }
