a0ce85802b98db5bbf0797fb38c8c9529fc32223
Oxumoron <oxymoron.dr@gmail.com>
2022-09-11 21:11:49 +0200

    add form

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ import { workDay } from './myObj.js';
 let day = [...workDay];
 
 const timeList = document.getElementById('day'),
-      tasks = timeList.querySelectorAll('.tasks__box');
+      tasks = timeList.querySelectorAll('.tasks__box'),
+      popup = document.getElementById('form__wrapper'),
+      start = document.getElementById('time-start');
 
 let min = [...tasks].map((task) => {
   return { id: task.id, from: task.id.split('-')[0], to: task.id.split('-')[1] };
@@ -23,3 +25,32 @@ day.forEach((item) => {
   newTask.style.top = `${item.start - min[findMin].from}px`;
   task.appendChild(newTask);
 });
+
+function getTimeFromMins(dayStart, mins) {
+  let hours = Math.trunc(mins/60);
+  let minutes = mins % 60;
+  return (hours+dayStart) + ':' + minutes;
+};
+
+timeList.addEventListener('click', (event) => {
+  // console.log(event.currentTarget, getTimeFromMins(event.clientY));
+  popup.classList.add('show');
+  
+  start.value = getTimeFromMins(8, event.clientY);
+  
+  popupHidden()
+})
+
+function popupHidden() {
+  popup.addEventListener('click', (event) => {
+    if(event.target.className == popup.className) {
+      popup.classList.remove('show');
+    }
+  });
+
+  document.addEventListener('keydown', function (e) {
+    if(e.keyCode === 27) {
+      popup.classList.remove('show');
+    }
+  });
+}
