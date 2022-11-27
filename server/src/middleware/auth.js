c751c29fda47574cba5edd860564b8e2ed5bb95b
Oxumoron <oxymoron.dr@gmail.com>
2022-11-27 16:14:40 +0100

    point

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -3,13 +3,15 @@ const key = require("../config/config")
 const config = key.jwt;
 
 const verifyToken = (req, res, next) => {
-  const token =
-    req.body.token || req.query.token || req.headers["x-access-token"];
-
-  if (!token) {
-    return res.status(403).send("A token is required for authentication");
-  }
   try {
+    const token =
+      req.body.token || req.cookies.myToken || req.headers["x-access-token"];
+
+    if (!token) {
+      return res.status(403).send("A token is required for authentication");
+    }
+    // jwt.verify(token, config);
+
     const decoded = jwt.verify(token, config);
     req.user = decoded;
   } catch (err) {
@@ -18,4 +20,21 @@ const verifyToken = (req, res, next) => {
   return next();
 };
 
-module.exports = verifyToken;
+// function checkToken(req, res, next) {
+//   try {
+//     const token = req.cookies.macOutletTOKEN;
+//     if (!token) {
+//       return res.status(403).send("A token is required for authentication");
+//     }
+//     jwt.verify(token, config);
+//     next()
+//   } catch (err) {
+//     console.log(err);
+//     return res.status(401).send("Invalid Token");
+//   }
+// }
+
+module.exports =
+  verifyToken
+// checkToken
+;
