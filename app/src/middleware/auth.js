facf9582caf9a43d39183089be64bf94b718ebde
Oxumoron <oxymoron.dr@gmail.com>
2022-11-28 18:25:38 +0100

    some fix for heroku

--- app/src/middleware/auth.js
+++ app/src/middleware/auth.js
@@ -2,42 +2,41 @@ const jwt = require("jsonwebtoken");
 const key = require("../config/config")
 const config = key.jwt;
 
+// const verifyToken = (req, res, next) => {
+//   try {
+//     const token = req.cookies.myToken;
+//     // req.headers["x-access-token"];
+
+//     if (!token) {
+//       return res.status(403).send("A token is required for authentication");
+//     }
+//     jwt.verify(token, config);
+
+//     // const decoded = jwt.verify(token, config);
+//     // req.user = decoded;
+//     // next();
+//   } catch (err) {
+//     return res.status(401).send("Invalid Token");
+//   }
+//   return next();
+// };
+
 const verifyToken = (req, res, next) => {
+  const token =
+    req.body.token || req.query.token || req.headers["x-access-token"];
+  // let token = req.cookies['x-access-token'];
+
+  // console.log(`token `, token);
+  if (!token) {
+    return res.status(403).send("A token is required for authentication");
+  }
   try {
-    const token = req.headers["x-access-token"];
-    // req.cookies.myToken;
-
-
-
-    if (!token) {
-      return res.status(403).send("A token is required for authentication");
-    }
-    // jwt.verify(token, config);
-
     const decoded = jwt.verify(token, config);
     req.user = decoded;
-    next();
   } catch (err) {
     return res.status(401).send("Invalid Token");
   }
   return next();
 };
 
-// function checkToken(req, res, next) {
-//   try {
-//     const token = req.cookies.macOutletTOKEN;
-//     if (!token) {
-//       return res.status(403).send("A token is required for authentication");
-//     }
-//     jwt.verify(token, config);
-//     next()
-//   } catch (err) {
-//     console.log(err);
-//     return res.status(401).send("Invalid Token");
-//   }
-// }
-
-module.exports =
-  verifyToken
-// checkToken
-;
+module.exports = verifyToken
