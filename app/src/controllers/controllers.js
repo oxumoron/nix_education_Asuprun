861d9fcf009cf988ef71cb1a3aa1d4fdba6c35e8
Oxumoron <oxymoron.dr@gmail.com>
2022-11-28 09:46:55 +0100

    some fix for heroku

--- app/src/controllers/controllers.js
+++ app/src/controllers/controllers.js
@@ -69,7 +69,9 @@ module.exports.userSign = async function login(req, res) {
 module.exports.getProducts = async function (req, res) {
   try {
     const items = await products.find();
-    res.status(200).sendFile(path.resolve('app/html/index.html')).json(items);
+    res.status(200).sendFile(path.resolve('app/html/index.html'));
+    console.log(items);
+    // res.status(200).sendFile(path.resolve('app/html/index.html')).json(items);
   } catch (e) {
     errorHandler(res, e)
   }
