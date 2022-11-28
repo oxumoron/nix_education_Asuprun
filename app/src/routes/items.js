37df014000ae96a39be37baf3d19fffc8cf88882
Oxumoron <oxymoron.dr@gmail.com>
2022-11-28 10:39:48 +0100

    some fix for heroku

--- app/src/routes/items.js
+++ app/src/routes/items.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const verifyToken = require('../middleware/auth')
 const {
   getProducts,
+  getFirstPage
 } = require('../controllers/controllers')
 const {
   searchProd
