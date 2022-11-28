76ad15b1fbbed48540721301fdc5ec24cf568bfb
Oxumoron <oxymoron.dr@gmail.com>
2022-11-28 18:33:01 +0100

    some fix for heroku

--- app/src/middleware/auth.js
+++ app/src/middleware/auth.js
@@ -2,41 +2,38 @@ const jwt = require("jsonwebtoken");
 const key = require("../config/config")
 const config = key.jwt;
 
+const verifyToken = (req, res, next) => {
+  try {
+    const token = req.cookies.myToken;
+    // req.headers["x-access-token"];
+
+    if (!token) {
+      return res.status(403).send("A token is required for authentication");
+    }
+    jwt.verify(token, config);
+
+    // const decoded = jwt.verify(token, config);
+    // req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).send("Invalid Token");
+  }
+};
+
 // const verifyToken = (req, res, next) => {
+//   const token =
+//     req.body.token || req.query.token || req.headers["x-access-token"];
+
+//   if (!token) {
+//     return res.status(403).send("A token is required for authentication");
+//   }
 //   try {
-//     const token = req.cookies.myToken;
-//     // req.headers["x-access-token"];
-
-//     if (!token) {
-//       return res.status(403).send("A token is required for authentication");
-//     }
-//     jwt.verify(token, config);
-
-//     // const decoded = jwt.verify(token, config);
-//     // req.user = decoded;
-//     // next();
+//     const decoded = jwt.verify(token, config);
+//     req.user = decoded;
 //   } catch (err) {
 //     return res.status(401).send("Invalid Token");
 //   }
 //   return next();
 // };
-
-const verifyToken = (req, res, next) => {
-  const token =
-    req.body.token || req.query.token || req.headers["x-access-token"];
-  // let token = req.cookies['x-access-token'];
-
-  // console.log(`token `, token);
-  if (!token) {
-    return res.status(403).send("A token is required for authentication");
-  }
-  try {
-    const decoded = jwt.verify(token, config);
-    req.user = decoded;
-  } catch (err) {
-    return res.status(401).send("Invalid Token");
-  }
-  return next();
-};
 
 module.exports = verifyToken
